d5b825a0ccd3504b5dd828c11caaca20d44572c9
Ruben Green <greenrl91@gmail.com>
2026-01-11 20:13:19 -0500

    Committing January 11th's 8KYU C# challenge

---
+++ January/C#/8KYU/January11thChallenge.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+
+public class January10thChallenge : MonoBehaviour{
+
+//Title : Convert a Number to a String!
+//Source : https://www.codewars.com/kata/5265326f5fda8eb1160004c8/csharp
+//Instructions
+
+/*
+    We need a function that can transform a number (integer) into a string.
+
+    What ways of achieving this do you know?  
+*/
+public string numToString(int num){
+    return num.ToString();
+}
+}
