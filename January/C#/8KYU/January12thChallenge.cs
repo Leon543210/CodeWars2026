0cfc8219926cf6674d92204abf67d707ebd01210
Ruben Green <greenrl91@gmail.com>
2026-01-12 20:00:42 -0500

    Committing January 12th's 8KYU C# challenge

---
+++ January/C#/8KYU/January12thChallenge.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+
+public class January12thChallenge : MonoBehaviour{
+ public static int Move(int position, int roll) //this line was provided.
+  {
+    //My solution - start
+    int newPosition = position + roll * 2;
+    
+    return newPosition;
+
+    // -end
+  	throw new NotImplementedException(); //this line was provided.
+    
+    
+  }
+}
