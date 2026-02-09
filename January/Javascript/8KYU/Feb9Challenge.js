5478a67a4d0d6bf6baf6f7b01b9c41a3cb1ad719
Ruben Green <greenrl91@gmail.com>
2026-02-09 17:58:58 -0500

    Committing Feb 9th JS 8KYU challenge+solution

---
+++ January/Javascript/8KYU/Feb9Challenge.js
@@ -0,0 +1,29 @@
+//Title: Are You Playing Banjo?
+
+//Instructions
+/*
+    Create a function which answers the question "Are you playing banjo?".
+    If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+    The function takes a name as its only argument, and returns one of the following strings:
+
+        name + " plays banjo" 
+        name + " does not play banjo"
+    Names given are always valid strings.
+*/
+
+//Source: https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+
+
+
+//My Solution
+function areYouPlayingBanjo(name) {
+  // Implement me
+  let input = name;
+  if(input[0] == 'r' || input[0] == 'R'){
+    return name + " plays banjo"
+  }
+  else{
+    return name + " does not play banjo";
+  }
+}
