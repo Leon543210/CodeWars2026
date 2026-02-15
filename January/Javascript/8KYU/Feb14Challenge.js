8811563f7908606202bc8f306ff4d630ad23c467
Ruben Green <greenrl91@gmail.com>
2026-02-14 23:44:07 -0500

    Submitting Feb 14th JS 8KYU challenge

---
+++ January/Javascript/8KYU/Feb14Challenge.js
@@ -0,0 +1,13 @@
+//Title: You Can't Code Under Pressure #1
+
+//Source: https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+//Instructions
+/*
+    Code as fast as you can! You need to double the integer and return it.
+*/
+
+//Solution
+function doubleInteger(i) {
+  return i * i;
+}
